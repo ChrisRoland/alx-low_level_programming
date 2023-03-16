3fa8bc0bcd1de4a79e9076a9ed2939dfca020c73
ChrisRoland <chrisebuberoland@gmail.com>
2023-03-16 16:41:03 -0700

    intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 1 on success
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
